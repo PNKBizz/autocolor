8b3e8329dd797649cec0769a5057ed159ce71a4f
PNKBizz <bizz.gc@gmail.com>
2017-08-26 21:31:38 +0300

    start creating app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <md-whiteframe md-elevation="0">
+    <md-layout md-gutter>
+      <md-layout>
+        Магазин
+      </md-layout>
+      <md-layout>
+        <div class="logo"></div>
+      </md-layout>
+      <md-layout>
+        Услуги
+      </md-layout>
+    </md-layout>
+  </md-whiteframe>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .logo {
+    width: 200px;
+    height: 200px;
+    background: url('./assets/logo.svg')
+  }
+</style>
