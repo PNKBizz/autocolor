82b118f9105681074cd04c7a44b38eb0c029e2bf
PNKBizz <bizz.gc@gmail.com>
2017-08-29 07:28:22 +0300

    add lazy irective

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <md-whiteframe md-elevation="0">
-    <md-layout md-gutter>
-      <md-layout>
-        Магазин
-      </md-layout>
-      <md-layout>
-        <div class="logo"></div>
-      </md-layout>
-      <md-layout>
-        Услуги
-      </md-layout>
-    </md-layout>
+<md-layout class="main">
+  <md-whiteframe md-tag="md-toolbar" :md-elevation="elevation" class="app md-transparent" 
+  :class="elevation ? 'md-dense' : 'md-large'">
+    <div class="md-toolbar-container">
+      <md-button class="md-icon-button" @click="$refs.sidenav.toggle()">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <span style="flex: 1"></span>
+
+      <md-button class="md-icon-button">
+        <md-icon>search</md-icon>
+      </md-button>
+
+      <md-button class="md-icon-button">
+        <md-icon>view_module</md-icon>
+      </md-button>
+    </div>
   </md-whiteframe>
+  <md-layout class="content"></md-layout>
+</md-layout>
 </template>
 
 <script>
@@ -19,13 +27,32 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      elevation: 0
     }
+  },
+  mounted() {
+    const self = this;
+    document.addEventListener('scroll', () => {
+      self.elevation = window.pageYOffset ? 5 : 0;
+    })
   }
 }
 </script>
 
 <style lang="scss">
+  body {
+    display: flex;
+  }
+  .app {
+    max-height: 300px;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+  }
+  .content {
+    background: url('./assets/back.jpg') center no-repeat;
+  }
   .logo {
     width: 200px;
     height: 200px;
